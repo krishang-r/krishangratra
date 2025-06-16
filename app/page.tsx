f33af4fa7c655903f9328fd1aad56eb3df7b182b
Krishang <112581575+krishang-r@users.noreply.github.com>
2025-06-16 21:33:33 +0530

    overflow fix

--- app/page.tsx
+++ app/page.tsx
@@ -8,7 +8,7 @@ import Skills from "@/components/Skills";
 
 export default function Home() {
   return (
-    <main >
+    <main className="overflow-x-hidden">
       <Hero />
       <About />
       <Projects />
