8b3556dd8a6f5ce072c1187d3d33d4fe63bd9433
Krishang <112581575+krishang-r@users.noreply.github.com>
2025-06-16 16:12:50 +0530

    Debug page tsx error

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,6 @@ import FooterContact from "@/components/Footer";
 import Hero from "@/components/Hero";
 import Projects from "@/components/Projects";
 import Skills from "@/components/Skills";
-import Image from "next/image";
 
 export default function Home() {
   return (
