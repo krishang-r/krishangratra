a05b0534b0b526edbb391c4b3b1e69946b6d2c44
Krishang <112581575+krishang-r@users.noreply.github.com>
2025-06-18 00:20:51 +0530

    About text size

--- components/About.tsx
+++ components/About.tsx
@@ -5,7 +5,7 @@ const About = () => {
   return (
     <div className='relative min-h-screen mt-5'>
         <ImageCarousel />
-        <div className='w-[70%] text-4xl text-secondary font-bold text-center m-auto'>
+        <div className='w-[70%] text-2xl md:text-4xl text-secondary font-bold text-center m-auto'>
             I’m someone who finds joy in solving real problems through thoughtful design and clean code. I care about the little details, the user experience, and building things that not only work — but feel good to use. My goal is to keep learning, stay curious, and create digital experiences that leave a positive mark.
         </div>
     </div>
