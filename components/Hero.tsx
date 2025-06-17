049e28a240fcd91f6da69207ba2f2e1ae6603b95
Krishang <112581575+krishang-r@users.noreply.github.com>
2025-06-17 11:07:57 +0530

    Hero button layout fix

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,8 +1,8 @@
-import React from 'react'
-import { BackgroundGradientAnimation } from './ui/BackgroundGradient'
-import { TextGenerateEffect } from './ui/TextGenerate'
-import { IoMdDownload } from "react-icons/io"
-import { IoIosSend } from "react-icons/io"
+import React from 'react';
+import { BackgroundGradientAnimation } from './ui/BackgroundGradient';
+import { TextGenerateEffect } from './ui/TextGenerate';
+import { IoMdDownload } from "react-icons/io";
+import { IoIosSend } from "react-icons/io";
 
 const Hero = () => {
   return (
@@ -20,25 +20,26 @@ const Hero = () => {
               Krishang Ratra
             </h1>
             <TextGenerateEffect
-              className='w-[50%] mx-auto'
-              words="Somewhere between caffeine and creativity, you'll find me building things that matter."
+              className='md:w-[50%] w-[90%] mx-auto' // Adjusted width for small screens
+              words="Somewhere between caffeine and caffeine and creativity, you'll find me building things that matter."
             />
-            <div className='w-[30%] mx-auto my-4 flex items-center justify-around text-center font-bold'>
-              <a href="/files/Resume.pdf" download>
-              <button className="p-[3px] relative w-[200px] cursor-pointer">
-                <div className="absolute inset-0 bg-gradient-to-r from-blue-400 to-blue-600 rounded-lg" />
-                <div className="px-8 py-2 bg-transparent rounded-[6px] relative group transition duration-200 text-white hover:text-xl">
-                  RESUME <span><IoMdDownload className='inline-block -mt-1 p-0' /></span>
-                </div>
-              </button>
+            {/* THIS IS THE CRUCIAL CHANGE FOR BUTTONS */}
+            <div className='flex flex-col md:flex-row items-center justify-center gap-4 my-4 font-bold max-w-full mx-auto p-4'>
+              <a href="/files/Resume.pdf" download className="w-full md:w-[200px]"> {/* Make link full width on small screens */}
+                <button className="p-[3px] relative w-full cursor-pointer"> {/* Button takes full width of its parent link */}
+                  <div className="absolute inset-0 bg-gradient-to-r from-blue-400 to-blue-600 rounded-lg" />
+                  <div className="px-8 py-2 bg-transparent rounded-[6px] relative group transition duration-200 text-white hover:text-xl">
+                    RESUME <span><IoMdDownload className='inline-block -mt-1 p-0' /></span>
+                  </div>
+                </button>
               </a>
-              <a href="mailto:ratrakrishang@gmail.com">
-              <button className="p-[3px] relative w-[200px] cursor-pointer">
-                <div className="absolute inset-0 bg-gradient-to-r from-blue-600 to-blue-400 rounded-lg" />
-                <div className="px-8 py-2 bg-transparent rounded-[6px] relative group transition duration-200 text-white hover:text-xl">
-                  MAIL <IoIosSend className='inline-block -mt-1 p-0' />
-                </div>
-              </button>
+              <a href="mailto:ratrakrishang@gmail.com" className="w-full md:w-[200px]"> {/* Make link full width on small screens */}
+                <button className="p-[3px] relative w-full cursor-pointer"> {/* Button takes full width of its parent link */}
+                  <div className="absolute inset-0 bg-gradient-to-r from-blue-600 to-blue-400 rounded-lg" />
+                  <div className="px-8 py-2 bg-transparent rounded-[6px] relative group transition duration-200 text-white hover:text-xl">
+                    MAIL <IoIosSend className='inline-block -mt-1 p-0' />
+                  </div>
+                </button>
               </a>
             </div>
           </div>
@@ -48,6 +49,6 @@ const Hero = () => {
       <div className="absolute bottom-0 left-0 w-full h-24 bg-gradient-to-b from-transparent to-white pointer-events-none z-10" />
     </div>
   )
-} 
+}
 
-export default Hero
+export default Hero;
